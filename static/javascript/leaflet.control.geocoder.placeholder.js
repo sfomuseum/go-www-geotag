f735d4d024fa5ab37f229a249c243776f4fc77c7
thisisaaronland <aaron@localhost>
2021-04-28 17:18:33 -0700

    snapshot: start testing with tangram

--- static/javascript/leaflet.control.geocoder.placeholder.js
+++ static/javascript/leaflet.control.geocoder.placeholder.js
@@ -1,93 +1,172 @@
-  var Placeholder = /*#__PURE__*/function () {
+// https://github.com/perliedman/leaflet-control-geocoder/
+// I am uncertain about nearly everything below because the docs for custom
+// plugins are all geared towards modern/compiled JavaScript so it's a bit
+// unclear how best to do things by hand (20210428/thisisaaronland)
+var Placeholder = function () {
+    
     function Placeholder(options) {
-      this.options = {
-        serviceUrl: 'http://localhost:3000'
-      };
-      this._lastSuggest = 0;
-      L.Util.setOptions(this, options);
+	this.options = {
+            serviceUrl: 'http://localhost:3000'
+	};
+	L.Util.setOptions(this, options);
     }
-
+    
     var _proto = Placeholder.prototype;
-
+    
     _proto.geocode = function geocode(query, cb, context) {
-      var _this = this;
-
-      var params {
-          text: query
-      });
 	
-      getJSON(this.options.serviceUrl + '/search', params, function (data) {
-        cb.call(context, _this._parseResults(data, 'bbox'));
-      });
+	var _this = this;
+
+	var hierarchy = [
+	    "neighbourhood",
+	    "locality",
+	    "county",
+	    "region",
+	    "country",
+	];
+	
+	var params = {
+            text: query
+	};
+	
+	this.getJSON(this.options.serviceUrl + '/parser/search', params, function (data){
+
+	    var results = [];
+
+		var count = data.length;
+		
+		for (var i=0; i < count; i++){
+		    
+		    var pl = data[i];
+		    var result = {};
+		    
+		    var bounds = pl.geom.bbox.split(",");
+		    
+		    var swlon = parseFloat(bounds[0]);		
+		    var swlat = parseFloat(bounds[1]);
+		    var nelon = parseFloat(bounds[2]);		
+		    var nelat = parseFloat(bounds[3]);
+		    
+		    bounds = L.latLngBounds([swlat, swlon], [nelat, nelon]);
+		    
+		    var placetype = pl.placetype;
+		    var lineage = pl.lineage[0];
+		    
+		    var names = []
+		    var ok = false;
+		    
+		    for (var j in hierarchy){
+			
+			var pt = hierarchy[j];
+			
+			if (pt == placetype){
+			    ok = true;
+			}
+			
+			if (!ok){
+			    continue
+			}
+
+			if ((pt == "county") && (names.length)){
+			    continue;
+			}
+			
+			if (! lineage[pt]){
+			    continue;
+			}
+			
+			if ((names.length > 0) && (pt == "region") && (lineage[pt].abbr)){
+			    names.push(lineage[pt].abbr);
+			    continue;
+			}
+			
+			names.push(lineage[pt].name);
+		    }
+		    
+		    var name = names.join(", ");
+		    
+		    if (name == ""){
+			name = pl.name;
+		    }
+		    
+		    name = name + " (" + placetype + ")";
+		    result.name = name;
+		    
+		    result.center = [ pl.geom.latitude, pl.geom.longitude ];
+		    result.bounds = bounds;
+		    
+		    results.push(result);		
+		}
+	    
+	    cb.call(context, results);
+	});
     };
 
     _proto.suggest = function suggest(query, cb, context) {
-      var _this2 = this;
-
-      var params = geocodingParams(this.options, {
-        api_key: this.options.apiKey,
-        text: query
-      });
-      getJSON(this.options.serviceUrl + '/autocomplete', params, function (data) {
-        if (data.geocoding.timestamp > _this2._lastSuggest) {
-          _this2._lastSuggest = data.geocoding.timestamp;
-          cb.call(context, _this2._parseResults(data, 'bbox'));
-        }
-      });
+	return this.geocode(query, cb, context);
     };
 
     _proto.reverse = function reverse(location, scale, cb, context) {
-      var _this3 = this;
-
-      var params = reverseParams(this.options, {
-        api_key: this.options.apiKey,
-        'point.lat': location.lat,
-        'point.lon': location.lng
-      });
-      getJSON(this.options.serviceUrl + '/reverse', params, function (data) {
-        cb.call(context, _this3._parseResults(data, 'bounds'));
-      });
+	cb.call(context, []);
+    };
+	 
+    _proto.getJSON = function(url, params, callback) {
+	
+	var xmlHttp = new XMLHttpRequest();
+	
+	xmlHttp.onreadystatechange = function () {
+	    if (xmlHttp.readyState !== 4) {
+		return;
+	    }
+	    
+	    var message;
+	    
+	    if (xmlHttp.status !== 200 && xmlHttp.status !== 304) {
+		message = '';
+	    } else if (typeof xmlHttp.response === 'string') {
+		// IE doesn't parse JSON responses even with responseType: 'json'.
+		    try {
+			message = JSON.parse(xmlHttp.response);
+		    } catch (e) {
+			// Not a JSON response
+			message = xmlHttp.response;
+		    }
+	    } else {
+		message = xmlHttp.response;
+	    }
+	    
+	    callback(message);
+	};
+	
+	xmlHttp.open('GET', url + this.getParamString(params), true);
+	xmlHttp.responseType = 'json';
+	xmlHttp.setRequestHeader('Accept', 'application/json');
+	xmlHttp.send(null);
     };
 
-      _proto._parseResults = function _parseResults(data, bboxname) {
-
-	  console.log("PARSE", data);
-	  return;
-	  
-      var results = [];
-      L.geoJSON(data, {
-        pointToLayer: function pointToLayer(feature, latlng) {
-          return L.circleMarker(latlng);
-        },
-        onEachFeature: function onEachFeature(feature, layer) {
-          var result = {};
-          var bbox;
-          var center;
-
-          if (layer.getBounds) {
-            bbox = layer.getBounds();
-            center = bbox.getCenter();
-          } else if (layer.feature.bbox) {
-            center = layer.getLatLng();
-            bbox = L.latLngBounds(L.GeoJSON.coordsToLatLng(layer.feature.bbox.slice(0, 2)), L.GeoJSON.coordsToLatLng(layer.feature.bbox.slice(2, 4)));
-          } else {
-            center = layer.getLatLng();
-            bbox = L.latLngBounds(center, center);
-          }
-
-          result.name = layer.feature.properties.label;
-          result.center = center;
-          result[bboxname] = bbox;
-          result.properties = layer.feature.properties;
-          results.push(result);
-        }
-      });
-      return results;
+    _proto.getParamString = function(obj, existingUrl, uppercase) {
+	  var params = [];
+	
+	for (var i in obj) {
+	    var key = encodeURIComponent(uppercase ? i.toUpperCase() : i);
+	    var value = obj[i];
+	    
+	    if (!Array.isArray(value)) {
+		params.push(key + '=' + encodeURIComponent(String(value)));
+	    } else {
+		for (var j = 0; j < value.length; j++) {
+		    params.push(key + '=' + encodeURIComponent(value[j]));
+		}
+	    }
+	}
+	
+	return (!existingUrl || existingUrl.indexOf('?') === -1 ? '?' : '&') + params.join('&');
     };
 
     return Placeholder;
-  }();
-  /**
+    
+}();
+/**
    * [Class factory method](https://leafletjs.com/reference.html#class-class-factories) for {@link Placeholder}
    * @param options the options
    */
